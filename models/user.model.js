41d422fbdbcdef1c66b2a3055c5e6620f8c61c46
Vaibhav Baneshi <vaibhavbaneshi@gmail.com>
2024-01-21 22:12:26 +0530

    create user model

---
+++ models/user.model.js
@@ -0,0 +1,26 @@
+import mongoose, {Schema} from "mongoose"
+
+const userSchema = new Schema (
+    {
+        username: {
+            type: String,
+            unique: true,
+            required: true
+        },
+
+        email: {
+            type: String,
+            unique: true,
+            required: true
+        },
+
+        password: {
+            type: String,
+            required: true
+        }
+    }, {timestamps: true}
+)
+
+const User = mongoose.model('User', userSchema)
+
+export default User
