d2600b54c5e19292b24437206cb4197c3d00c821
Vaibhav Baneshi <vaibhavbaneshi@gmail.com>
2024-01-21 13:45:54 +0530

    create pages and route

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,24 @@
-import { useState } from 'react'
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+import DashBoard from "./pages/DashBoard"
+import SignIn from "./pages/SignIn"
+import SignUp from "./pages/SignUp"
+import About from "./pages/About"
+import Home from "./pages/Home"
+import Projects from "./pages/Projects"
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  
   return (
-    <>
-      <h1 className='text-3xl text-red-500'>MERN APP</h1>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/about" element={<About/>}/>
+        <Route path="/dashboard" element={<DashBoard/>}/>
+        <Route path="/signup" element={<SignUp/>}/>
+        <Route path="/signin" element={<SignIn/>}/>
+        <Route path="/projects" element={<Projects/>}/>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
