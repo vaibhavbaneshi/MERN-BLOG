4c37c21305cb106197f4bce113c4e68c7492b677
Vaibhav Baneshi <vaibhavbaneshi@gmail.com>
2024-01-28 15:50:56 +0530

    update API route

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -2,6 +2,8 @@ import User from "../models/user.model.js"
 import { errorHandler } from "../utils/ApiError.js"
 import { ApiResponse } from "../utils/ApiResponse.js"
 import {asyncHandler} from "../utils/asyncHandler.js"
+import bcryptjs from "bcryptjs"
+
 
 export const test = (req, res) => {
     res.json({message: "Api is working!"})
@@ -9,32 +11,32 @@ export const test = (req, res) => {
 
 export const updateUser = asyncHandler ( async (req, res, next) => {
     if(req.user._id !== req.params.userId) {
-        return next(errorHandler(403, "You are not allowed to update current user"))
+        return next(errorHandler(403, false, "You are not allowed to update current user"))
     }
 
     if (req.body.password) {
         if (req.body.password.length < 6) {
-          return next(errorHandler(400, 'Password must be at least 6 characters'));
+          return next(errorHandler(400, false, 'Password must be at least 6 characters'));
         }
         req.body.password = bcryptjs.hashSync(req.body.password, 10);
       }
     
     if(req.body.username) {
         if(req.body.username.length < 7 || req.body.username.length > 20) {
-            return next(errorHandler(400, "Username must be between 7 and 20 characters"))
+            return next(errorHandler(400, false, "Username must be between 7 and 20 characters"))
         }
     }
 
     if (req.body.username.includes(' ')) {
-        return next(errorHandler(400, 'Username cannot contain spaces'));
+        return next(errorHandler(400, false, 'Username cannot contain spaces'));
     }
 
     if (req.body.username !== req.body.username.toLowerCase()) {
-    return next(errorHandler(400, 'Username must be lowercase'));
+    return next(errorHandler(400, false, 'Username must be lowercase'));
     }
 
     if (!req.body.username.match(/^[a-zA-Z0-9]+$/)) {
-    return next(errorHandler(400, 'Username can only contain letters and numbers'));
+    return next(errorHandler(400, false, 'Username can only contain letters and numbers'));
     }
 
     const updatedUser = await User.findByIdAndUpdate(
